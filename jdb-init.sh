abc615fd8910f1f2bc1f0717f04b86dcbfb72332
Ignatyev Vjacheslav <ivv.private@gmail.com>
2010-09-29 11:38:23 +0400

    Setup jdb

--- jdb-init.sh
+++ jdb-init.sh
@@ -1,28 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 HOME=/home/ivv
 BASE_NAME=${HOME}/projects
 
+SOURCE_DIRS=$(mktemp)
+
+# findup sources
+locate -e "${BASE_NAME}*\.java" \
+| grep -v '\/\(examples\|test\|samples\|tests\)\/' \
+| xargs -n1 -P0 awk '/^\t*package/ { sub(";",""); num=match(FILENAME, $2); print substr(FILENAME, 1, num - 2); exit}'  \
+| sort | uniq | sed '/^$/ d' > ${SOURCE_DIRS}
+
+# setup jdb
 sed -i '/use/ d' $HOME/.jdbrc
 
-# SRC=$(locate -e "${BASE_NAME}*\.java" \
-# | xargs -n1 -P0 awk '/^\t*package/ { sub(";",""); num=match(FILENAME, $2); print substr(FILENAME, 1, num - 2); exit}'  \
-# | sort | uniq | tr '\n' ':' ) 
+{ 
+    echo -n 'use '
+    cat ${SOURCE_DIRS} | tr '\n' ':'
+    echo ''
+} >>  $HOME/.jdbrc
 
-# mkfifo $TMP_FIFO
-# locate -b '*\.java' | sed 's/\/[^\/]*\java$/\*/' | uniq > $TMP_FIFO &
 
-# for i in $( locate "${BASE_NAME}*\.java" | sed 's/\/[^\/]*\java$/\*/' | uniq )
-# do 
-#  fn=$(find $i -iname '*\.java' | tail -n1)
-#  awk '/^\t*package/ { sub(";",""); num=match(FILENAME, $2); print substr(FILENAME, 1, num - 2); exit}' $fn
-# done
-
-# rm $TMP_FIFO
-
-# find -iname '*\.java' /home/ivv/projects/shark-persist/src/org/comsoft/sharkimpl/repository/model 
-
-SRC=$(locate -e "${BASE_NAME}*\.java" \
-| xargs -n1 -P0 awk '/^\t*package/ { sub(";",""); num=match(FILENAME, $2); print substr(FILENAME, 1, num - 2); exit}'  \
-| sort | uniq | tr '\n' ':' ) 
-echo use $SRC >> $HOME/.jdbrc
+# setup emacs jdb
+{ 
+    echo "'(gud-jdb-directories (quote ("
+    sed 's/.*/"\0"/' ${SOURCE_DIRS}
+    echo ')))'
+} > $HOME/.emacs.d/gud-jdb-directories.el
