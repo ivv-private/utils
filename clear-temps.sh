6da2195c6813978fc57f9febea882e1c6ac990be
Ignatyev Vjacheslav <ivv.private@gmail.com>
2011-01-22 12:19:12 +0300

    obsolete

--- clear-temps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/bin/rm -rf /home/ivv/temp-midnight/*
-# /bin/rm -rf /mnt/public/share/temp-midnight/*
-# /usr/bin/find /mnt/public/share/temp-daily  -atime +1 -mindepth 1 -print0 | /usr/bin/xargs -0 -r /bin/rm -rf
-# /usr/bin/find /mnt/public/share/temp-weekly -atime +7 -mindepth 1 -print0 | /usr/bin/xargs -0 -r /bin/rm -rf
